24e930d1c7c7952ee7fd1c7d2ba2b9b1fc17ac68
Olayinka Abubakar Sadiq <sadiqolayinka17@gmail.com>
2025-01-01 23:18:30 +0000

    fixed error

--- src/components/experienceTimeline.tsx
+++ src/components/experienceTimeline.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ExperienceTimeline = () => {
     const experiences = [
       {
--- src/components/servicesSection.tsx
+++ src/components/servicesSection.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ServicesSection = () => {
   const services = [
     {
